416c6ba7dea48833cf5712b12a9be7f200379f3d
drewbeathard <andrewpatrick97@gmail.com>
2021-11-30 08:59:34 -0700

    11/30

--- cpp_full/cpp_full.cpp
+++ cpp_full/cpp_full.cpp
@@ -22,7 +22,10 @@
 #include <pcl/common/transforms.h>
 #include <pcl/visualization/pcl_visualizer.h>
 
-typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+#include <pcl/common/io.h> // for concatenateFields
+
+
+//typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
 int main(int argc, char **argv)
 {
@@ -40,12 +43,18 @@ int main(int argc, char **argv)
     bool have_pointcloud = false;
     bool have_odom = false;
     int counter = 0;
-    int big_step = 40; // use this to keyframe values 
+    int bigCounter = 0;
+    int cloudCounter = 0;
+    int big_step = 100; // use this to keyframe values 
     int small_step = 1;
     int iterator = big_step;
     //pcl::PointCloud<pcl::PointXYZ>  global_pointcloud;
     pcl::PointCloud<pcl::PointXYZ>::Ptr global_pointcloud (new pcl::PointCloud<pcl::PointXYZ> ());
-    
+    pcl::PointCloud<pcl::PointXYZ>::Ptr concat_pointcloud (new pcl::PointCloud<pcl::PointXYZ> ());
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pc1 (new pcl::PointCloud<pcl::PointXYZ> ());
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pc2 (new pcl::PointCloud<pcl::PointXYZ> ());
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pc3 (new pcl::PointCloud<pcl::PointXYZ> ());
+   
 
 
     foreach(rosbag::MessageInstance const m, view)
@@ -70,19 +79,19 @@ int main(int argc, char **argv)
                     have_pointcloud = true;
                     iterator = small_step; //(1)
                     std_msgs::Header hp = input_pointcloud->header;
-                    std::cout << hp << std::endl;
+                    //std::cout << hp << std::endl;
 
                 }    
             } 
 
             if (input_odom != NULL) // if we get odom, THIS IS WHERE WE SHOULD DO MATRIX TRANSFORM! 
             {
-                if (have_pointcloud == true)
+                if (have_pointcloud == true) // if we have a recent matching pointcloud ready to use
                 {
                     std_msgs::Header ho = input_odom->header;
-                    std::cout<<ho<<std::endl <<std::endl << std::endl; //all at once
+                    //std::cout<<ho<<std::endl <<std::endl << std::endl; //all at once
                     have_pointcloud = false;
-                    iterator = big_step;
+                    iterator = big_step; // change iterator back -> skip a number of messages 
 
 
 
@@ -110,98 +119,78 @@ int main(int argc, char **argv)
 
                     // // Print the transformation
                     // printf ("\nMethod #2: using an Affine3f\n");
-                    std::cout << transform.matrix() << std::endl;
+                    //std::cout << transform.matrix() << std::endl;
 
                     // // Executing the transformation
                     pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud (new pcl::PointCloud<pcl::PointXYZ> ());
                     pcl::transformPointCloud (*global_pointcloud, *transformed_cloud, transform);
+                    
+                    
+                    std::cout << "Cloud counter: " << cloudCounter << std::endl;
 
+                    if (cloudCounter == 0) *pc1 = *global_pointcloud;
+                    if (cloudCounter == 20) *pc2 = *global_pointcloud;
+                    if (cloudCounter == 2) *pc3 = *global_pointcloud;
+                    // now we have 3 different pointclouds to plot
 
-                    // Visualization
-                    printf(  "\nPoint cloud colors :  white  = original point cloud\n"
-                        "                        red  = transformed point cloud\n");
-                    pcl::visualization::PCLVisualizer viewer ("Matrix transformation example");
+                    // if (cloudCounter == 0)
+                    // {
+                    //     *concat_pointcloud = *global_pointcloud;
+                    // }
+                    // else
+                    // {
+                    //     //std::cout << "global pointcloud width and height: " << global_pointcloud -> width << " " << global_pointcloud -> height << std::endl;
+                    //     *concat_pointcloud = (*concat_pointcloud) + (*global_pointcloud); // add it to the huge pointcloud 
 
-                     // Define R,G,B colors for the point cloud
-                    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> source_cloud_color_handler (global_pointcloud, 255, 255, 255);
-                    // We add the point cloud to the viewer and pass the color handler
-                    viewer.addPointCloud (global_pointcloud, source_cloud_color_handler, "original_cloud");
-
-                    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> transformed_cloud_color_handler (transformed_cloud, 230, 20, 20); // Red
-                    viewer.addPointCloud (transformed_cloud, transformed_cloud_color_handler, "transformed_cloud");
-
-                    viewer.addCoordinateSystem (1.0, "cloud", 0);
-                    viewer.setBackgroundColor(0.05, 0.05, 0.05, 0); // Setting background to a dark grey
-                    viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "original_cloud");
-                    viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "transformed_cloud");
-                    //viewer.setPosition(800, 400); // Setting visualiser window position
-
-                    while (!viewer.wasStopped ()) { // Display the visualiser until 'q' key is pressed
-                        viewer.spinOnce ();
-  }
-
-
-
-
-
-
-
-
-
+                    //     // concatenate pointclouds (concat pointcloud + global pointcloud)
+                    // }
+                    // std::cout << "concat pointcloud width and height: " << concat_pointcloud -> width << " " << concat_pointcloud -> height << std::endl;
+                    // std::cout << "global pointcloud width and height: " << global_pointcloud -> width << " " << global_pointcloud -> height << std::endl;
+                    cloudCounter++;
 
                 }
-            }
-
-
-
-
-            // at this point we have a PCD point cloud and and an odom message
-
-
-
-
-
-            //get header info for pointcloud
-            // std_msgs::Header hp = input_pointcloud->header;
-            //  std::cout<<hp<<std::endl; //all at once
-            // std::cout<< "Timestamp: " << hp.stamp<<std::endl; //specific parts of it
-            // //std::cout<<h.stamp.sec<<std::endl;
-            // //std::cout<<h.stamp.nsec<<std::endl;
-            // std::cout<< "Sequence: " << hp.seq<<std::endl;
-            
-            //get header info for odometry
-            // std_msgs::Header ho = input_odom->header;
-            // std::cout<<ho<<std::endl; //all at once
-            //std::cout<< "Timestamp: " << ho.stamp<<std::endl; //specific parts of it
-            //std::cout<<h.stamp.sec<<std::endl;
-            //std::cout<<h.stamp.nsec<<std::endl;
-            //std::cout<< "Sequence: " << ho.seq<<std::endl;
-
-
-            
-            //pcl::PointCloud<pcl::PointXYZ> output_pointcloud;
-            //pcl::fromROSMsg(*input_pointcloud, output_pointcloud);
-            
-            //float x = output_pointcloud.points[0].x;
-            //std::cout << x << std::endl;
-
-
-            // PLAN
-
-            // take each pointcloud as it comes in in -> convert it to PCD file format  
-            // find the matching odometry based off of time 
-            // skip the filtering part - go straight to 
-                // matrix transform
-                // concatenate the pointclouds   
-                
-                    
-           
+            } 
 
         }
 
         counter++;
+        bigCounter++;
     }
-    
+
+    // print out concat_pointcloud information:
+    std::cout << "global pointcloud width and height: " << global_pointcloud -> width << " " << global_pointcloud -> height << std::endl;
+
+    std::cout << "Big counter: " << bigCounter << std::endl;
+    std::cout << "Cloud counter: " << cloudCounter << std::endl;
+
+    std::cout << concat_pointcloud -> width << " " << concat_pointcloud -> height << std::endl;
+
+    // Visualization
+    printf(  "\nPoint cloud colors :  white  = original point cloud\n"
+        "                        red  = transformed point cloud\n");
+    pcl::visualization::PCLVisualizer viewer ("Matrix transformation example");
+
+    // Define R,G,B colors for the point cloud
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> source_cloud_color_handler (pc2, 255, 255, 255);
+    // We add the point cloud to the viewer and pass the color handler
+    viewer.addPointCloud (pc2, source_cloud_color_handler, "pc2");
+
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> transformed_cloud_color_handler (pc1, 230, 20, 20); // Red
+    viewer.addPointCloud (pc1, transformed_cloud_color_handler, "pc1");
+
+    viewer.addCoordinateSystem (1.0, "cloud", 0);
+    viewer.setBackgroundColor(0.05, 0.05, 0.05, 0); // Setting background to a dark grey
+    viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "pc2");
+    viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "pc1");
+    //viewer.setPosition(800, 400); // Setting visualiser window position
+
+    while (!viewer.wasStopped ()) { // Display the visualiser until 'q' key is pressed
+        viewer.spinOnce ();
+    }
+
+
+
+
     bag.close();
 
     return 0;
